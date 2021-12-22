b1e1379480ce4bfb03745ad5d901468ca8ec9766
YesCoach <sbsii1020@gmail.com>
2021-12-23 04:12:14 +0900

    feat: Delete Storyboard, Add VCs for Tab bar Delete Info.plist's keys about Storyboard name Set Root ViewController to UIWIndow at AppDelegate Add two viewControllers, one is for tableView, another is for ScrollView

---
+++ Project 01 - GoodAsOldPhones/GoodAsOldPhones/GoodAsOldPhones/VC/ScrollViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by 박태현 on 2021/12/23.
+//
+
+import UIKit
+
+class ScrollViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Us"
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Project 01 - GoodAsOldPhones/GoodAsOldPhones/GoodAsOldPhones/VC/TableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by 박태현 on 2021/12/23.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Products"
+        // Do any additional setup after loading the view.
+    }
+}
