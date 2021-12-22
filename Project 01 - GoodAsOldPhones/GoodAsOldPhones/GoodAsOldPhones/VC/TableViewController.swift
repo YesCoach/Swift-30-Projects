5cefe9238f098a4327e171d756c00744443313a8
YesCoach <sbsii1020@gmail.com>
2021-12-23 04:45:06 +0900

    refactor: fix code for setting rootViewController Move code for setting rootViewController from Appdelegate to SceneDelegate. Add simple tableView to TableViewController

--- Project 01 - GoodAsOldPhones/GoodAsOldPhones/GoodAsOldPhones/VC/TableViewController.swift
+++ Project 01 - GoodAsOldPhones/GoodAsOldPhones/GoodAsOldPhones/VC/TableViewController.swift
@@ -9,9 +9,37 @@ import UIKit
 
 class TableViewController: UIViewController {
 
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Products"
         // Do any additional setup after loading the view.
+        setUILayout()
     }
+
+    func setUILayout() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 8).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 8).isActive = true
+    }
+}
+
+extension TableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension TableViewController: UITableViewDelegate {
+    
 }
